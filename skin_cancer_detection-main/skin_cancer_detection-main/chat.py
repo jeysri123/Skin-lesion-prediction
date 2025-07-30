96f0e2a42178b1d8a6945d0b346015ecfbf36211
JEYSRI <141422901+jeysri123@users.noreply.github.com>
2025-07-30 11:37:03 +0530

    Add files via upload

---
+++ skin_cancer_detection-main/skin_cancer_detection-main/chat.py
@@ -0,0 +1,54 @@
+import google.generativeai as genai
+import os
+import dotenv
+import PIL.Image
+import os
+
+dotenv.load_dotenv()
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+# Configure Gemini
+genai.configure(api_key=GEMINI_API_KEY)
+
+# Shared message history
+messages = []
+
+
+
+
+def init_prompt(desease):
+    messages.append({"role": "user", "parts": f"""Imagine you are an AI Expert in analying Skin deseases.
+                    Your job is to Interact with the user to confirm if they really have the given skin desease. 
+                    In case it's an healthy skin, say the skin looks healthy.
+                    If it's a skin desease, ask for any symptoms ask atleast 2-3 questions to confirm the desease.
+                    Remember the given desease is provided by analyzing a image, your job is to confirm if the user really has the desease or not.
+                    If the symptoms doesn't match the desease, ask the user to consult a doctor.
+                    ASK ONE QUESTION AT A TIME.
+                    Desease: {desease}
+                    Question:
+                    """})
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(messages)
+    messages.append({"role": "model", "parts": [response.text]})
+    print(response.text)
+    return response.text
+
+def generate_response(prompt):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    messages.append({"role": "user", "parts": prompt})
+    response = model.generate_content(messages)
+    messages.append({"role": "model", "parts": [response.text]})
+    return response.text
+
+
+def process_img():
+    try:
+        img = PIL.Image.open('image.jpg')
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content(["""your job is to analyze the given image and say if you suspect any skin deseases or not. If the given image is not a skin, say "not a skin". If you think it's a healthy skin, say "healthy skin". If NOT then provide the name of desease you think it is. don't give any other information. Look at the image clearly befor responding. Response:  """, img], stream=True)
+        response.resolve()
+        print(response.text)
+        return response.text
+    except Exception as e:
+        print(e)
+        return e
---
+++ skin_cancer_detection-main/skin_cancer_detection-main/main.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from PIL import Image
+import io
+import os
+from chat import process_img, generate_response, init_prompt
+
+st.title(" 😶‍🌫️ Skin Disease AI")
+
+if "uploaded_image" not in st.session_state:
+    st.session_state.uploaded_image = None
+
+file_uploader_placeholder = st.empty()
+camera_input_placeholder = st.empty()
+
+if st.session_state.uploaded_image is None:
+    uploaded_file = file_uploader_placeholder.file_uploader("🖼️ Upload an image", type=["jpg", "png"])
+    picture = camera_input_placeholder.camera_input("📸Or take a picture")
+
+    if uploaded_file is not None:
+        picture = uploaded_file
+        
+
+    if picture:
+        image = Image.open(picture)
+        st.session_state.uploaded_image = image
+        image.save('image.jpg')
+
+        file_uploader_placeholder.empty()
+        camera_input_placeholder.empty()
+
+if st.session_state.uploaded_image is not None:
+    st.image(st.session_state.uploaded_image, caption='Uploaded Image', use_column_width=True)
+    disease = process_img()
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": init_prompt(disease)}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    def clear_chat_history():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+    if prompt := st.chat_input(placeholder="Type a message"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = generate_response(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for text in response:
+                    full_response += text
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        message = {"role": "assistant", "content": full_response}
+        st.session_state.messages.append(message)
